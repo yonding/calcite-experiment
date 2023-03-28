e3b394df4147806f8c030f5f6b3497dda5c9d052
yonding <uwrgoy7584@khu.ac.kr>
2023-03-28 14:52:00 +0900

    feat : connect to mongodb

--- src/main/java/com/example/calciteexperiment/MongoConnection.java
+++ src/main/java/com/example/calciteexperiment/MongoConnection.java
@@ -7,6 +7,18 @@ import org.bson.Document;
 
 public class MongoConnection {
     public void connectToMongo(){
+        String uri = "mongodb+srv://uwrgoy7584:Rlarkdud5762!~!@cluster0.htn8ooq.mongodb.net/?retryWrites=true&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("myFirstDatabase");
+            System.out.println("1");
+            MongoCollection<Document> collection = database.getCollection("user");
+            Document doc = collection.find(eq("username", "Kayoung")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
     }
 
 }
